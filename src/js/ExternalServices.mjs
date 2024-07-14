c986646157ab8d66b68efea10450f8311ea51627
Orlando Ancelmo Pinelo <orlandoancelmopinelo@gmail.com>
2024-07-13 22:54:51 -0600

    second  commit netlify no working using jsonfile

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,38 +1,38 @@
-const baseURL = import.meta.env.VITE_SERVER_URL;
+//const baseURL = import.meta.env.VITE_SERVER_URL;
 
  // Closing curly brace added here
 //import { dbdata } from "./db"; 
-//import { leagusdb } from "./leaguedb";
+import { leagusdb } from "./leaguedb";
 
 
- async function convertToJson(res) {
-   const jsonResponse = await res.json();
- if (res.ok) {
-      return jsonResponse;
-    } else {
-      throw { name: "servicesError", message: jsonResponse };
-    }
-  }
+ //async function convertToJson(res) {
+   //const jsonResponse = await res.json();
+ //if (res.ok) {
+  //    return jsonResponse;
+    //} else {
+     // throw { name: "servicesError", message: jsonResponse };
+    //}
+  //}
 
 export default class ExternalServices {
 
   async getLeaguesData(){
 
     
-//    const leagueData = leagusdb;
-  //  return leagueData
+    const leagueData = leagusdb;
+    return leagueData
 
-     const options = {
-                   method: "GET",
-                    headers: {
-                          "x-rapidapi-key": "f03592658791198d69b990b1526e4f78",
-                           "x-rapidapi-host": "v3.football.api-sports.io"
-                       }
-                 };
+    // const options = {
+      //             method: "GET",
+        //            headers: {
+          //                "x-rapidapi-key": "f03592658791198d69b990b1526e4f78",
+            //               "x-rapidapi-host": "v3.football.api-sports.io"
+              //         }
+                // };
         
-          const response = await fetch(baseURL + `leagues`, options);
-          const data = await convertToJson(response);
-          return data; 
+          //const response = await fetch(baseURL + `leagues`, options);
+          //const data = await convertToJson(response);
+          //return data; 
 
    }
   
